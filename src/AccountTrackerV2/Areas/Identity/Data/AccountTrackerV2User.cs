87c5ab4f3c2ac23305cec5e2331a58119d3bf11e
Mark Taylor <Mark.Taylor737@gmail.com>
2020-09-20 15:59:55 -0400

    Initial Commit. Includes Identity schema.

---
+++ src/AccountTrackerV2/Areas/Identity/Data/AccountTrackerV2User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AccountTrackerV2.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AccountTrackerV2User class
+    public class AccountTrackerV2User : IdentityUser
+    {
+        [Required]
+        [PersonalData]
+        public string FirstName { get; set; }
+
+        [Required]
+        [PersonalData]
+        public string LastName { get; set; }
+
+        [Required]
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ src/AccountTrackerV2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AccountTrackerV2.Areas.Identity.Data;
+using AccountTrackerV2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AccountTrackerV2.Areas.Identity.IdentityHostingStartup))]
+namespace AccountTrackerV2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AccountTrackerV2UserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AccountTrackerV2UserContextConnection")));
+
+                services.AddDefaultIdentity<AccountTrackerV2User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AccountTrackerV2UserContext>();
+            });
+        }
+    }
+}
